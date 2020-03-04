f3d9ae65310bbbd8bda7322c1dac2a58124f3b28
Itamar Gozlan <ItamarGIP@csf.technion.ac.il>
2020-03-04 10:07:46 +0200

    Post run push - TRIPLETS MODE - auto push - TEST

--- source/train_main.bat
+++ source/train_main.bat
@@ -1,5 +1,5 @@
 python C:/Users/ItamarGIP/PycharmProjects/Banana-Learning/Banana-Learning/source/native_cnn.py 1> C:/Users/ItamarGIP/PycharmProjects/Banana-Learning/Banana-Learning/source/logs/native_main_run.log 2> C:/Users/ItamarGIP/PycharmProjects/Banana-Learning/Banana-Learning/source/logs/native_main_run_err.log
 python C:/Users/ItamarGIP/PycharmProjects/Banana-Learning/Banana-Learning/source/transfer_learning.py 1>C:/Users/ItamarGIP/PycharmProjects/Banana-Learning/Banana-Learning/source/logs/tl_main_run.log 2> C:/Users/ItamarGIP/PycharmProjects/Banana-Learning/Banana-Learning/source/logs/tl_main_run_err.log
 git add .
-git commit -m "Post run push - data augmantation enabled - auto push"
-git push origin master
+git commit -m "Post run push - TRIPLETS MODE - auto push - TEST"
+git push origin HEAD
